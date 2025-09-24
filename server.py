58e583c027c026f6b53e6c8181858bfaad1d7c88
Md. Inan Ahsan <inanahsan@gmail.com>
2025-09-25 03:23:52 +0600

    updated server code to prevent misuse

--- server.py
+++ server.py
@@ -4,9 +4,15 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit import print_formatted_text
 from prompt_toolkit.formatted_text import ANSI
 from prompt_toolkit.patch_stdout import patch_stdout
+import os
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 session = PromptSession()
+if not os.path.exists('ip_names.json'):
+    # Create a new file with empty JSON
+    with open('ip_names.json', "w") as f:
+        json.dump({}, f)
 port = 12345
 connections = {}
 used_colors = {}
@@ -24,6 +30,8 @@ colors = [
     "\033[94m",
 ]
 
+ip_name_map = {}
+
 my_color = "\033[95m"
 
 s.bind(("", port))
@@ -34,13 +42,24 @@ def send_to_all(msg, excAddr = ""):
         c = connections[addr]
         if excAddr and excAddr == addr:
             continue
-        c.sendall(msg.encode())
+        # c.sendall(msg.encode())
+        try:    
+            c.sendall(msg.encode())
+        except:
+            c.close()
+            connections.pop(addr)
 
 def send_message():
     with patch_stdout():
         try:
             while True:
                 msg = session.prompt()
+                if not msg:
+                    continue
+                if msg == "___read_names___":
+                    with open('ip_names.json', "r") as f:
+                        ip_name_map.update(json.load(f))
+                    continue
                 send_to_all(f"{my_color}{username}: {msg}\033[0m")
         except:
             s.close()
@@ -49,10 +68,14 @@ def receive_msg(addr):
     while True:
         c = connections[addr]
         msg = c.recv(1024).decode()
+        ipaddr = addr.split('_')[0]
         if not msg:
             c.close()
             connections.pop(addr)
             break
+        if ipaddr in ip_name_map:
+            colon_position = msg.find(':')
+            msg = f"{ip_name_map[ipaddr]}{msg[colon_position:]}"
         if addr in used_colors:
             msg = f"{used_colors[addr]}{msg}\033[0m"
         print_formatted_text(ANSI(msg))
@@ -73,7 +96,7 @@ try:
             c.close()
             break
         print(f"Got connection from {addr} with username {uname}",)
-
+        send_to_all(f"{addr} joined the chat with username {uname}", f"{addr[0]}_{addr[1]}")
         c.sendall(f"you are {uname}".encode())
         t2 = threading.Thread(target=receive_msg, args=(f"{addr[0]}_{addr[1]}",))
         t2.start()
